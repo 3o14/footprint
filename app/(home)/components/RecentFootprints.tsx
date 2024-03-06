6472b8ec97248e8e6a55c741a3b35516eb06bb6d
이원주 <dldnjswn4265@gmail.com>
2024-03-06 17:44:51 +0900

    style/#6 홈페이지 최근에 달린 발자국 UI 구현

--- app/(home)/components/RecentFootprints.tsx
+++ app/(home)/components/RecentFootprints.tsx
@@ -1,9 +1,19 @@
+import { FaPaw } from "react-icons/fa";
+
 const RecentFootprints = () => {
-  return (
-    <div>
-      최근에 달린 발자국
-    </div>
-  );
+	let footrpints = ["12.13", "12.13", "12.13", "12.13", "12.13", "12.13"];
+	return (
+		<section className="flex flex-col items-start px-7 pt-10 h-1/3 w-screen">
+			<p className="text-xl font-bold">최근에 달린 발자국</p>
+			<ul className="w-full overflow-x-auto whitespace-nowrap overflow-y-hidden flex gap-4 pt-5">
+				{footrpints.map((footrprint, index) => (
+					<li key={index}>
+						<FaPaw size="4rem" className="text-[#88AB8E]" />
+					</li>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default RecentFootprints;
